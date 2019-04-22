4c6305ca9fcbc0f301f54eb8fdcb41c5b0786451
Jacob Senteney <jwsenteney@gmail.com>
2019-04-22 01:54:38 -0400

    Positioning main window directly underneath tray icon (mac)

--- index.js
+++ index.js
@@ -20,10 +20,23 @@ app.on("ready", () => {
     process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new Tray(iconPath);
-  tray.on("click", () => {
+
+  tray.on("click", (event, bounds) => {
+    // Click event bounds
+    const { x, y } = bounds;
+
+    // Window height and width
+    const { height, width } = mainWindow.getBounds();
+
     if (mainWindow.isVisible()) {
       mainWindow.hide();
     } else {
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y,
+        height,
+        width
+      });
       mainWindow.show();
     }
   });
